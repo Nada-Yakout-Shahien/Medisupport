4939ab8c7d7c141177f4651d9b2bf5b85febbc8d
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-01-28 16:42:48 +0200

    add-disease

--- src/pages/Smart_health_metrics.js
+++ src/pages/Smart_health_metrics.js
@@ -2,6 +2,8 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import { Helmet } from "react-helmet-async";
 import React from "react";
+import  "./Smart_health_metrics.css";
+
 
 const SmartHealthMetrics = () => {
   return (
@@ -12,7 +14,23 @@ const SmartHealthMetrics = () => {
       </Helmet>
 
       <Header />
+      <div className="disease">
+        <h3>Smart Health Metrics</h3>
+        <div className="child">
+          <div className="recording">
+            
+          </div>
+          <div className="recording">
 
+          </div>
+          <div className="recording">
+
+          </div>
+          <div className="recording">
+
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
