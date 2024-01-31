dd18625bc2b6fc3fe3dff5e077b7b2a07afc60cb
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-01-31 18:21:10 +0200

    add

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,11 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
 import { Helmet } from "react-helmet-async";
+import heart from "../images/heart.png";
+import { NavLink } from "react-router-dom";
+import "./home.css";
 
-function Home(){
+function Home() {
   return (
     <>
       <Helmet>
@@ -12,17 +15,29 @@ function Home(){
 
       <Header />
 
-      {/* <div className="welcome">
-        <img src="" alt="" />
-        <h3>Welcome to MediSupport Website</h3>
-        <h4>Heart disease prediction</h4>
-        <p>Proin quis cras euismod sit et metus risus ut. Semper nam vel morbi sit cursus tincidunt massa et a. Dolor odio parturient cursus justo nunc enim, a, sit facilisi. Eleifend at ac lacus, ullamcorper mauris eget tortor mollis.</p>
-        <button>Record Now</button>
-      </div> */}
+      <div className="homesec1">
+        <div className="leftpart">
+          <img src={heart} alt="" />
+        </div>
+
+        <div className="rightpart">
+          <h2>Welcome to <span className="medi">MediSupport</span> Website</h2>
+          <h3>Heart disease prediction</h3>
+          <p>
+            Proin quis cras euismod sit et metus risus ut. Semper nam vel morbi
+            sit cursus tincidunt massa et a. Dolor odio parturient cursus justo
+            nunc enim, a, sit facilisi. Eleifend at ac lacus, ullamcorper mauris
+            eget tortor mollis.
+          </p>
+          <NavLink to="/welcome" className="btn">
+            Record Now
+          </NavLink>
+        </div>
+      </div>
 
       <Footer />
     </>
   );
-};
+}
 
 export default Home;
