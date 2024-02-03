ed0ccca5b58dc7c87f6a98a492c0b07011cd36c5
HossamEzzat921 <hossamezzat921@gmail.com>
2024-02-03 02:48:16 +0200

    P_add_img

--- src/pages/ar_pressure.js
+++ src/pages/ar_pressure.js
@@ -3,6 +3,7 @@ import Footer from "../components/footer";
 import { Helmet } from "react-helmet-async";
 import "./ar_disease.css";
 import React from "react";
+import Pimg from "../images/ar_img_p.png"
 
 const ArPressure = () => {
   return (
@@ -12,8 +13,16 @@ const ArPressure = () => {
         <meta name="description" content="Article Pressure" />
       </Helmet>
       <Header />
-      
-      <Footer />
+      <div className="bg_article">
+        <h3 className="address">Blood pressure</h3>
+        <img className="P_img" src={Pimg} alt="" />
+        <div className="topic">
+          <p className="s_topic"></p>
+          <p className="s_topic"></p>
+          <p className="s_topic"></p>
+        </div>
+      </div>
+      {/* <Footer /> */}
     </>
   );
 };
