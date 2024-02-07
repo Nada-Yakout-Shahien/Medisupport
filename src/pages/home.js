5963038777739ecd9950aa1c5a960d3ba93fe4e4
Nadasobhy <nadasobhy984@gmail.com>
2024-02-07 20:08:16 +0200

    ad

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,22 @@
 import { Helmet } from "react-helmet-async";
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import "./home.css";
+import Layout from '../components/Layout';
 import heart from "../images/heart.png";
 import heartrate from "../images/heart-rate.png";
 import bloodpressure from "../images/blood-pressure.png";
 import bloodsugar from "../images/blood-sugar.png";
 import bmi from "../images/bmi.png";
 import arrow from "../images/ri-arrow-for-article.svg";
-import { NavLink } from "react-router-dom";
-import "./home.css";
-import Layout from '../components/Layout';
-
 const Home = () => {
   return (
-    <Layout>
+    <layout>
       <Helmet>
         <title>Medisupport HOME ♥</title>
         <meta name="description" content="HOME" />
       </Helmet>
-
-      <Header />
-
+                      {/* homesec1 */}
       <div className="homesec1">
         <div className="leftpart">
           <img src={heart} alt="" />
@@ -40,8 +38,171 @@ const Home = () => {
         </div>
       </div>
 
-      <Footer />
-    </>
+                            {/* homesec2*/}
+
+      <div className="smarthealth-sec">
+        <div className="nameh">
+          <h3>Smart Health Metrics</h3>
+            <NavLink to="/Smart_health_metrics" className="trans">
+              <p>See all</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="31"
+                height="40"
+                viewBox="0 0 41 40"
+                fill="none">
+                <g clip-path="url(#clip0_1_2137)">
+                  <path
+                    d="M27.6272 18.3333L18.4637 9.39329L20.8793 7.03662L34.1667 20L20.8793 32.9633L18.4637 30.6066L27.6272 21.6666H6.83337V18.3333H27.6272Z"
+                    fill="#1F1F1F"/>
+                </g>
+                <defs>
+                  <clipPath id="clip0_1_2137">
+                    <rect width="41" height="40" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+            </NavLink>
+        </div>
+        <div className="childh">
+          <div className="recording">
+            <div className="infh">
+              <h4>Heart Rate</h4>
+              <div className="num">
+                <h3>65</h3>
+                <p className="ex">PBM</p>
+              </div>
+            </div>
+            <img src={heartrate} alt="" />
+          </div>
+          <div className="recording">
+            <div className="infh">
+              <h4>Blood Pressure</h4>
+              <div className="num">
+                <h3>140/90</h3>
+                <p className="ex">mmHg</p>
+              </div>
+            </div>
+            <img src={bloodpressure} alt="" />
+          </div>
+          <div className="recording">
+            <div className="infh">
+              <h4>Blood Sugar</h4>
+              <div className="num">
+                <h3>120</h3>
+                <p className="ex">mg/dl</p>
+              </div>
+            </div>
+            <img src={bloodsugar} alt="" />
+          </div>
+          <div className="recording">
+            <div className="infh">
+              <h4>BMI</h4>
+              <div className="num">
+                <h3>80</h3>
+                <p className="ex">kg</p>
+              </div>
+            </div>
+            <img src={bmi} alt="" />
+          </div>
+        </div>
+      </div>
+
+                              {/* homesec3 */}
+
+       <div className="articles-sec">
+        <div className="word">
+          Articles
+          <NavLink to="/Reed_articles" className="trans">
+            <p>See all</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="31"
+              height="40"
+              viewBox="0 0 41 40"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_1_2137)">
+                <path
+                  d="M27.6272 18.3333L18.4637 9.39329L20.8793 7.03662L34.1667 20L20.8793 32.9633L18.4637 30.6066L27.6272 21.6666H6.83337V18.3333H27.6272Z"
+                  fill="#1F1F1F"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_1_2137">
+                  <rect width="41" height="40" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </NavLink>
+        </div>
+        <div className="diss">
+          <div className="disease">
+            <p>Blood pressure</p>
+            <NavLink to="/pressure">
+              <img src={arrow} alt="" />
+            </NavLink>
+          </div>
+        </div>
+        <div className="diss">
+          <div className="disease">
+            <p>Blood sugar</p>
+            <NavLink to="/sugar">
+              <img src={arrow} alt="" />
+            </NavLink>
+          </div>
+        </div>
+        <div className="diss">
+          <div className="disease">
+            <p>BMI</p>
+            <NavLink to="/bmi">
+              <img src={arrow} alt="" />
+            </NavLink>
+          </div>
+        </div>
+        <div className="diss">
+          <div className="disease">
+            <p>Heart</p>
+            <NavLink to="/heart">
+              <img src={arrow} alt="" />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      
+                            {/* homesec4 */}
+      
+      <div className="doc-sec">
+        <div className="deff">
+          <h3>Meet our doctors</h3>
+          <NavLink to="/doctors" className="trans">
+            <p>See all</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="31"
+              height="40"
+              viewBox="0 0 41 40"
+              fill="none"
+            >
+            <g clip-path="url(#clip0_1_2137)">
+              <path
+                d="M27.6272 18.3333L18.4637 9.39329L20.8793 7.03662L34.1667 20L20.8793 32.9633L18.4637 30.6066L27.6272 21.6666H6.83337V18.3333H27.6272Z"
+                fill="#1F1F1F"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1_2137">
+                <rect width="41" height="40" fill="white" />
+              </clipPath>
+            </defs>
+            </svg>
+          </NavLink>
+        </div>
+      </div>
+
+
+
+    </layout>
   );
 };
 
