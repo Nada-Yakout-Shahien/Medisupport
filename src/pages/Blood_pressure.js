c3babb4ec0bceb850bedc61c23b5e39dc7930136
HossamEzzat921 <hossamezzat921@gmail.com>
2024-02-10 18:47:12 +0200

    edit BP

--- src/pages/Blood_pressure.js
+++ src/pages/Blood_pressure.js
@@ -1,19 +1,32 @@
 import { Helmet } from "react-helmet-async";
 import "./Blood_pressure.css";
 import React from "react";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 const Bloodpressure = () => {
-  return (
-    <Layout>
-      <Helmet>
-        <title>Blood Pressure ♥</title>
-        <meta name="description" content="Blood Pressure" />
-      </Helmet>
+    return (
+        <Layout>
+            <Helmet>
+                <title> Blood Pressure♥ </title>{" "}
+                <meta name="description" content="Blood Pressure" />
+            </Helmet>{" "}
+            <div className="BP-page">
+                <div className="BP-address">
+                    <p>Bloode Pressure</p>
+                </div>
+                <div className="BP-status">
+                    <p>Average  <span>140/90</span> mmHG</p>
+                    <button><p>Normal</p></button>
+                </div>
+                <div className="BP-datr">
 
+                </div>
+                <div className="BP-data">
 
-    </Layout>
-  );
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default Bloodpressure;
