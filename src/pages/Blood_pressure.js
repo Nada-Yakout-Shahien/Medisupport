9b467da6ae098ff890cdb18250b0a8725470182a
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-02-10 19:19:56 +0200

    date

--- src/pages/Blood_pressure.js
+++ src/pages/Blood_pressure.js
@@ -1,32 +1,76 @@
 import { Helmet } from "react-helmet-async";
 import "./Blood_pressure.css";
-import React from "react";
+import React, { useState} from "react";
+import { eachDayOfInterval, format } from "date-fns";
 import Layout from "../components/Layout";
 
+//date show
+const generateDays = (startDate, numberOfDays) => {
+  const endDate = new Date(startDate.getTime());
+  endDate.setDate(startDate.getDate() + numberOfDays - 1);
+  return eachDayOfInterval({ start: startDate, end: endDate }).map((day) => ({
+    day: format(day, "EEE"),
+    date: format(day, "d"),
+    fullDate: format(day, "yyyy-MM-dd"),
+    color: "var(--Red)",
+    textColor: "var(--whiteColor)",
+  }));
+};
+
 const Bloodpressure = () => {
-    return (
-        <Layout>
-            <Helmet>
-                <title> Blood Pressure♥ </title>{" "}
-                <meta name="description" content="Blood Pressure" />
-            </Helmet>{" "}
-            <div className="BP-page">
-                <div className="BP-address">
-                    <p>Bloode Pressure</p>
-                </div>
-                <div className="BP-status">
-                    <p>Average  <span>140/90</span> mmHG</p>
-                    <button><p>Normal</p></button>
-                </div>
-                <div className="BP-datr">
+  //days
+  const startDate = new Date(2024, 5, 1);
+  const days = generateDays(startDate, 100);
 
-                </div>
-                <div className="BP-data">
+  //on click day
+  const [selectedDay, setSelectedDay] = useState(null);
 
+  const handleDayClick = (fullDate) => {
+    setSelectedDay(fullDate);
+  };
+
+  return (
+    <Layout>
+      <Helmet>
+        <title> Blood Pressure♥ </title>{" "}
+        <meta name="description" content="Blood Pressure" />
+      </Helmet>{" "}
+      <div className="BP-page">
+        <div className="BP-address">
+          <p>Bloode Pressure</p>
+        </div>
+        <div className="BP-status">
+          <p>
+            Average <span>140/90</span> mmHG
+          </p>
+          <button>
+            <p>Normal</p>
+          </button>
+        </div>
+
+        <div className="BP-date">
+          {days.map((item, index) => (
+            <div key={index} className="day-container">
+              <div className="day-name">{item.day}</div>
+              <div
+                className={`date-container ${
+                  selectedDay === item.fullDate ? "selected" : ""
+                }`}
+                onClick={() => handleDayClick(item.fullDate)}
+                style={{ background: item.color }}
+              >
+                <div className="date-text" style={{ color: item.textColor }}>
+                  {item.date}
                 </div>
+              </div>
             </div>
-        </Layout>
-    );
+          ))}
+        </div>
+
+        <div className="BP-data"></div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Bloodpressure;
