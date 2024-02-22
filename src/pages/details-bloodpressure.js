8475501faf98fd377a69204a506bbbea454ea785
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-02-22 04:09:47 +0200

    add

--- src/pages/details-bloodpressure.js
+++ src/pages/details-bloodpressure.js
@@ -70,15 +70,17 @@ const DetailsBloodpressure = () => {
               </div>
             ))}
           </div>
-          
-          <div className="dBP-diagram"></div>
-          
+
+          <div className="dBP-diagram">
+            
+          </div>
+
           <div className="inf-det">
             <h3>Recommended Reading</h3>
             <h4>How To Loss Sugar ?</h4>
             <p>
-              Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-              in laying out print, graphic or web designs. The passage is
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print, graphic or web designs. The passage is
               attributed to an unknown typesetter in the 15th century who is
               thought to have scrambled parts of Cicero's De Finibus Bonorum et
               Malorum for use in a type specimen book. It usually begins with:
