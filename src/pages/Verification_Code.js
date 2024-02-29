3b24de8a136b576a03615b8fd8773d6ee02f3993
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-02-29 14:57:40 +0200

    add

--- src/pages/Verification_Code.js
+++ src/pages/Verification_Code.js
@@ -5,36 +5,39 @@ import { NavLink } from "react-router-dom";
 import picture from "../images/picture.png";
 
 const VerificationCode = () => {
-    return (
-      <>
-        <Helmet>
-          <title>VerificationCode ♥</title>
-          <meta name="description" content="Verification_Code" />
-        </Helmet>
+  return (
+    <>
+      <Helmet>
+        <title>VerificationCode ♥</title>
+        <meta name="description" content="Verification_Code" />
+      </Helmet>
 
-        <div className="rverification">
+      <div className="rverification">
         <div classNEmail className="ver2">
           <img src={picture} alt="verication" />
         </div>
         <div className="ver1">
-            <div className="rrorr">
-          <h2>Verification Code</h2>
-          
-          <p>Please enter the verification code that we’ve sent to your phone.</p>
+          <div className="rrorr">
+            <h2>Verification Code</h2>
+
+            <p>
+              Please enter the verification code that we’ve sent to your phone.
+            </p>
           </div>
           <div className="rowr">
-            <input  type="number"/>
-            <input type="number"/>
-            <input type="number"/>
-            <input type="number"/>
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
+            <input type="number" />
           </div>
           <div className="buttonr">
-          <NavLink to="/New_Password" className="rbtsd">Verify</NavLink>
+            <NavLink to="/New_Password" className="rbtsd">
+              Verify
+            </NavLink>
           </div>
         </div>
       </div>
-                
-        </>
+    </>
   );
 };
 
