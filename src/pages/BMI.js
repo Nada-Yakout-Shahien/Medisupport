2331ad54b32eb9f3b0da9f6f5051345e604cf7ad
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-03-07 14:29:32 +0200

    ad

--- src/pages/BMI.js
+++ src/pages/BMI.js
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import "./BMI.css";
 import React from "react";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
 const BMI = () => {
   return (
@@ -10,17 +10,14 @@ const BMI = () => {
         <title>BMI ♥</title>
         <meta name="description" content="BMI" />
       </Helmet>
-       <div className="bmi_r">
-       <div className="bmir">
-         <h2 className="rbmi__r">BMI</h2>
-         </div>
-       <div className="rbodyr">
-        <p className="bmi_p">Gender</p>
-       </div>
-       
-       </div>
-       
-      
+      <div className="bmi_r">
+        <div className="bmir">
+          <h2 className="rbmi__r">BMI</h2>
+        </div>
+        <div className="rbodyr">
+          <p className="bmi_p">Gender</p>
+        </div>
+      </div>
     </Layout>
   );
 };
